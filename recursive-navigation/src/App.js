aa2dc226cbf085f51e4d50f03999286000203bd5
George Lynch <osu4life75@gmail.com>
2024-07-11 21:10:31 -0500

    Building components for menu

---
+++ recursive-navigation/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import TreeView from './components';
+import menus from './components/data'
+
+function App() {
+  return (
+    <div className="App">
+      <TreeView menus={menus}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ recursive-navigation/src/components/menu-item.jsx
@@ -0,0 +1,7 @@
+import MenuList from "./menu-list"
+
+export default function MenuItem({item}) {
+
+    return <div>Menu Item</div>
+    
+}
